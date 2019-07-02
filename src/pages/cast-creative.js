451506493b0e0675b42cbb00bc4a0f88928dc7a1
Andre Gibbs <andregibbs@Digital-Studio.local>
2019-07-02 12:34:20 +0100

    updates to cast page

--- src/pages/cast-creative.js
+++ src/pages/cast-creative.js
@@ -27,10 +27,6 @@ const CastAndCreativePage = ({data}) => (
                         </Row>
                     </Container>
 
-                     <Container>
-                        <h2 className="title py-4 text-uppercase text--black smaller-header">FURTHER CAST AND CREATIVE TEAM TO COME!</h2>
-                    </Container>
-
                     {/*<Container className="py-5">
                         <h2 className="title py-0 text-uppercase text--red smaller-header">Creative</h2>
                         <Row className="justify-content-center">
