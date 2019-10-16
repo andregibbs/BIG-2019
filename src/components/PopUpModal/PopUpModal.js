435e5bd457585808cdf152b8acbfd00b38543d29
Andre Gibbs <andregibbs@Digital-Studio.local>
2019-10-16 14:44:48 +0100

    updates

--- src/components/PopUpModal/PopUpModal.js
+++ src/components/PopUpModal/PopUpModal.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import {Modal, ModalBody} from 'reactstrap'
 
+const cookies = new Cookies();
+const COOKIE_DOMAIN = "bigthemusical.co.uk";
+let expiry = new Date();
+expiry = new Date(expiry.setMinutes(expiry.getMinutes() + 30))
+
+cookies.remove('offerDetails', {path: '/get-a-quote/', domain: COOKIE_DOMAIN});
+
+cookies.set('offer', 'no-offer', {
+    path: '/get-a-quote/',
+    expires: expiry,
+    domaain: COOKIE_DOMAIN
+});
+
+cookies.get('offerDetails');
+
 const PopUpModal = (props) => (
     <Modal isOpen={props.isOpen} className="popup-modal" toggle={props.togglePopModal}
            centered={true}>
